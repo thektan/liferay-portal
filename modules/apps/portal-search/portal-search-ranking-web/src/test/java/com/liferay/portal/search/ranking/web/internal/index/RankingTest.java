a98071601b642b8d3113630392179e29f7a8e48d
André de Oliveira <andre.oliveira@liferay.com>
2019-05-10 16:25:28 -0700

    LPS-89524 portal-search-ranking-web: Tests: Add Back-end Tests
    https://issues.liferay.com/browse/LPS-89524
    
    Co-authored-by: Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
    Co-authored-by: Bryan Engler <bryan.engler@liferay.com>
    Co-authored-by: Filipe Oshiro <filipe.oshiro@liferay.com>
    Co-authored-by: Kevin Tan <kevin.tan@liferay.com>
    Co-authored-by: Olivia Yu <olivia.yu@liferay.com>
    Co-authored-by: weicao <wade.cao@liferay.com>
    Co-authored-by: André de Oliveira <andre.oliveira@liferay.com>

---
+++ modules/apps/portal-search/portal-search-ranking-web/src/test/java/com/liferay/portal/search/ranking/web/internal/index/RankingTest.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.ranking.web.internal.index;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author André de Oliveira
+ */
+public class RankingTest {
+
+	@Test
+	public void testDefaults() {
+		Ranking ranking = new Ranking();
+
+		Assert.assertEquals("[]", String.valueOf(ranking.getAliases()));
+		Assert.assertEquals("[]", String.valueOf(ranking.getHiddenIds()));
+		Assert.assertEquals("[]", String.valueOf(ranking.getPins()));
+	}
+
+}
