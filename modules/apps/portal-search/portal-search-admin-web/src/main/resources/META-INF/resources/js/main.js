f17ceef1432302959fa15ce73f19bc4581bc7a7f
Carlos Lancha <carlos.lancha@liferay.com>
2019-08-26 12:31:56 +0200

    LPS-100106 - Replace A.io with fetch

--- modules/apps/portal-search/portal-search-admin-web/src/main/resources/META-INF/resources/js/main.js
+++ modules/apps/portal-search/portal-search-admin-web/src/main/resources/META-INF/resources/js/main.js
@@ -174,68 +174,73 @@ AUI.add(
 					);
 
 					if (currentAdminIndexPanel) {
-						A.io.request(instance.get(STR_URL), {
-							on: {
-								success: function(event, id, obj) {
-									var responseDataNode = A.Node.create(
-										this.get('responseData')
+						Liferay.Util.fetch(
+							instance.get(STR_URL),
+							{
+								method: 'POST'
+							}
+						)
+						.then(response => {
+							return response.text();
+						})
+						.then(response => {
+							var responseDataNode = A.Node.create(
+								response
+							);
+
+							var responseAdminIndexPanel = responseDataNode.one(
+								instance.get(STR_INDEX_ACTIONS_PANEL)
+							);
+
+							var responseAdminIndexNodeList = responseAdminIndexPanel.all(
+								'.index-action-wrapper'
+							);
+
+							var currentAdminIndexNodeList = currentAdminIndexPanel.all(
+								'.index-action-wrapper'
+							);
+
+							currentAdminIndexNodeList.each(function(
+								item,
+								index
+							) {
+								var inProgress = item.one('.progress');
+
+								var responseAdminIndexNode = responseAdminIndexNodeList.item(
+									index
+								);
+
+								if (!inProgress) {
+									inProgress = responseAdminIndexNode.one(
+										'.progress'
 									);
-
-									var responseAdminIndexPanel = responseDataNode.one(
-										instance.get(STR_INDEX_ACTIONS_PANEL)
-									);
-
-									var responseAdminIndexNodeList = responseAdminIndexPanel.all(
-										'.index-action-wrapper'
-									);
-
-									var currentAdminIndexNodeList = currentAdminIndexPanel.all(
-										'.index-action-wrapper'
-									);
-
-									currentAdminIndexNodeList.each(function(
-										item,
-										index
-									) {
-										var inProgress = item.one('.progress');
-
-										var responseAdminIndexNode = responseAdminIndexNodeList.item(
-											index
-										);
-
-										if (!inProgress) {
-											inProgress = responseAdminIndexNode.one(
-												'.progress'
-											);
-										}
-
-										if (inProgress) {
-											item.replace(
-												responseAdminIndexNode
-											);
-										}
-									});
-
-									var controlMenuId =
-										'#' + instance.ns('controlMenu');
-
-									var currentControlMenu = A.one(
-										controlMenuId
-									);
-
-									var responseControlMenu = responseDataNode.one(
-										controlMenuId
-									);
-
-									if (
-										currentControlMenu &&
-										responseControlMenu
-									) {
-										currentControlMenu.replace(
-											responseControlMenu
-										);
-									}
 								}
+
+								if (inProgress) {
+									item.replace(
+										responseAdminIndexNode
+									);
+								}
+							});
+
+							var controlMenuId =
+								'#' + instance.ns('controlMenu');
+
+							var currentControlMenu = A.one(
+								controlMenuId
+							);
+
+							var responseControlMenu = responseDataNode.one(
+								controlMenuId
+							);
+
+							if (
+								currentControlMenu &&
+								responseControlMenu
+							) {
+								currentControlMenu.replace(
+									responseControlMenu
+								);
 							}
 						});
 					}
@@ -255,7 +260,6 @@ AUI.add(
 	{
 		requires: [
 			'aui-io-plugin-deprecated',
-			'aui-io-request',
 			'liferay-portlet-base',
 			'querystring-parse'
 		]
