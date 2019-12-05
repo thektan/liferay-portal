4e95139224ede80cd7eb7b7a4c69d06fe864f385
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2019-12-05 17:20:59 -0300

    fixup! LPS-101102 portal-search-tuning-rankings-web: Display duplicate aliases in error message

--- modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/main/java/com/liferay/portal/search/tuning/rankings/web/internal/portlet/action/ValidateRankingMVCResourceCommand.java
+++ modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/main/java/com/liferay/portal/search/tuning/rankings/web/internal/portlet/action/ValidateRankingMVCResourceCommand.java
@@ -27,6 +27,7 @@ import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.search.index.IndexNameBuilder;
 import com.liferay.portal.search.searcher.SearchRequestBuilderFactory;
 import com.liferay.portal.search.tuning.rankings.web.internal.constants.ResultRankingsPortletKeys;
 import com.liferay.portal.search.tuning.rankings.web.internal.index.DuplicateQueryStringsDetector;
@@ -122,6 +123,9 @@ public class ValidateRankingMVCResourceCommand implements MVCResourceCommand {
 	protected DuplicateQueryStringsDetector duplicateQueryStringsDetector;
 
 	@Reference
+	protected IndexNameBuilder indexNameBuilder;
+
+	@Reference
 	protected Portal portal;
 
 	@Reference
@@ -176,7 +180,7 @@ public class ValidateRankingMVCResourceCommand implements MVCResourceCommand {
 		if (Validator.isBlank(index)) {
 			long companyId = portal.getCompanyId(resourceRequest);
 
-			index = "liferay-" + companyId;
+			index = indexNameBuilder.getIndexName(companyId);
 		}
 
 		return index;
