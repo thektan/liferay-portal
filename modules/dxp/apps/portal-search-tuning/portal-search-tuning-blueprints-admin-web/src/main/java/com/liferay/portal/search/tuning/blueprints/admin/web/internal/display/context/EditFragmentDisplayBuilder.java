b6fd38d171377cbcc868dd156b494e7b693f9f29
Kevin Tan <kevin.tan@liferay.com>
2021-01-07 10:48:21 -0800

    LPS-123115 portal-search-tuning-blueprints-admin-web: Formatting

--- modules/dxp/apps/portal-search-tuning/portal-search-tuning-blueprints-admin-web/src/main/java/com/liferay/portal/search/tuning/blueprints/admin/web/internal/display/context/EditFragmentDisplayBuilder.java
+++ modules/dxp/apps/portal-search-tuning/portal-search-tuning-blueprints-admin-web/src/main/java/com/liferay/portal/search/tuning/blueprints/admin/web/internal/display/context/EditFragmentDisplayBuilder.java
@@ -17,7 +17,6 @@ package com.liferay.portal.search.tuning.blueprints.admin.web.internal.display.c
 import com.liferay.exportimport.kernel.exception.NoSuchConfigurationException;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.language.Language;
@@ -26,25 +25,20 @@ import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.HashMapBuilder;
-import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.search.tuning.blueprints.admin.web.internal.constants.BlueprintsAdminMVCCommandNames;
 import com.liferay.portal.search.tuning.blueprints.admin.web.internal.constants.BlueprintsAdminWebKeys;
 import com.liferay.portal.search.tuning.blueprints.constants.BlueprintTypes;
-import com.liferay.portal.search.tuning.blueprints.engine.parameter.ParameterDefinition;
 import com.liferay.portal.search.tuning.blueprints.engine.util.BlueprintsEngineContextHelper;
 import com.liferay.portal.search.tuning.blueprints.model.Blueprint;
 import com.liferay.portal.search.tuning.blueprints.service.BlueprintService;
 
-import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.ResourceBundle;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionURL;
@@ -61,7 +55,9 @@ public class EditFragmentDisplayBuilder {
 	public EditFragmentDisplayBuilder(
 		HttpServletRequest httpServletRequest, Language language, Log log,
 		JSONFactory jsonFactory, RenderRequest renderRequest,
-		RenderResponse renderResponse, BlueprintsEngineContextHelper blueprintsEngineContextHelper, BlueprintService blueprintService) {
+		RenderResponse renderResponse,
+		BlueprintsEngineContextHelper blueprintsEngineContextHelper,
+		BlueprintService blueprintService) {
 
 		_httpServletRequest = httpServletRequest;
 		_language = language;
@@ -160,10 +156,6 @@ public class EditFragmentDisplayBuilder {
 			"submitFormURL", _getSubmitFormURL()
 		).build();
 
-		ResourceBundle resourceBundle =
-			ResourceBundleUtil.getModuleAndPortalResourceBundle(
-				_themeDisplay.getLocale(), getClass());
-
 		if (_blueprint != null) {
 			props.put(
 				"initialConfigurationString", _blueprint.getConfiguration());
@@ -171,8 +163,9 @@ public class EditFragmentDisplayBuilder {
 			props.put("initialTitle", _getTitleJSONObject());
 			props.put(
 				"predefinedVariables",
-				_blueprintsEngineContextHelper
-					.getContributedParameterDefinitionsJSONArray(_themeDisplay.getLocale()));
+				_blueprintsEngineContextHelper.
+					getContributedParameterDefinitionsJSONArray(
+						_themeDisplay.getLocale()));
 		}
 
 		return props;
