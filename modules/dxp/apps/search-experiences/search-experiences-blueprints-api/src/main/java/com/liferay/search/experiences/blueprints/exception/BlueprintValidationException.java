d93a651afa05e5969d0b29f8d78c0c5b0ca823df
André de Oliveira <andre.oliveira@liferay.com>
2021-08-24 19:17:33 -0500

    LPS-124256 search-experiences-blueprints-api: Initial commit
    https://issues.liferay.com/browse/LPS-124256
    
    Co-authored-by: Petteri Karttunen <petteri.karttunen@liferay.com>
    Co-authored-by: Adam Brandizzi <adam.brandizzi@liferay.com>
    Co-authored-by: Bryan Engler <bryan.engler@liferay.com>
    Co-authored-by: Joshua Cords <joshua.cords@liferay.com>
    Co-authored-by: Kevin Tan <kevin.tan@liferay.com>
    Co-authored-by: Olivia Yu <olivia.yu@liferay.com>
    Co-authored-by: Wade Cao <wade.cao@liferay.com>
    Co-authored-by: André de Oliveira <andre.oliveira@liferay.com>

---
+++ modules/dxp/apps/search-experiences/search-experiences-blueprints-api/src/main/java/com/liferay/search/experiences/blueprints/exception/BlueprintValidationException.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.search.experiences.blueprints.exception;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.search.experiences.problems.Problem;
+
+import java.util.List;
+
+/**
+ * @author Brian Wing Shun Chan
+ * @author Petteri Karttunen
+ */
+public class BlueprintValidationException extends PortalException {
+
+	public BlueprintValidationException() {
+	}
+
+	public BlueprintValidationException(String msg) {
+		super(msg);
+	}
+
+	public BlueprintValidationException(String msg, List<Problem> problems) {
+		super(msg);
+
+		_problems = problems;
+	}
+
+	public BlueprintValidationException(String msg, Throwable throwable) {
+		super(msg, throwable);
+	}
+
+	public BlueprintValidationException(Throwable throwable) {
+		super(throwable);
+	}
+
+	public List<Problem> getProblems() {
+		return _problems;
+	}
+
+	private static final long serialVersionUID = 1L;
+
+	private List<Problem> _problems;
+
+}
