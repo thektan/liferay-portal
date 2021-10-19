79130c9f8d5c9fb2d5a2be91bbbd4584c311bbe1
Olivia Yu <olivia.yu@liferay.com>
2021-10-19 16:09:08 -0700

    LPS-112856 search-experiences-web: Add edit SXPElement components

---
+++ modules/dxp/apps/search-experiences/search-experiences-web/src/main/resources/META-INF/resources/js/edit_sxp_element/index.js
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+import {fetch} from 'frontend-js-web';
+import React, {useEffect, useState} from 'react';
+
+import ErrorBoundary from '../shared/ErrorBoundary';
+import ThemeContext from '../shared/ThemeContext';
+import {DEFAULT_ERROR} from '../utils/constants';
+import {openErrorToast} from '../utils/toasts';
+import EditSXPElementForm from './EditSXPElementForm';
+
+export default function ({context}) {
+	const [resource, setResource] = useState(null);
+
+	const url = new URL(window.location.href);
+	const sxpElementId = url.searchParams.get(
+		`${context.namespace}sxpElementId`
+	);
+
+	useEffect(() => {
+		fetch(`/o/search-experiences-rest/sxp-elements/${sxpElementId}`, {
+			method: 'POST',
+		})
+			.then((response) => {
+				if (!response.ok) {
+					throw DEFAULT_ERROR;
+				}
+
+				return response.json();
+			})
+			.then((responseContent) => {
+				setResource(responseContent);
+			})
+			.catch((error) => {
+				openErrorToast();
+
+				if (process.env.NODE_ENV === 'development') {
+					console.error(error);
+				}
+
+				setResource({});
+			});
+	}, []); //eslint-disable-line
+
+	if (!resource) {
+		return null;
+	}
+
+	return (
+		<ThemeContext.Provider value={context}>
+			<div className="edit-sxp-element-root">
+				<ErrorBoundary>
+					<EditSXPElementForm
+						initialConfigurationString={
+							resource.initialConfigurationString
+						}
+						initialDescription={resource.initialDescription}
+						initialTitle={resource.initialTitle}
+						predefinedVariables={resource.predefinedVariables}
+						readOnly={resource.readOnly}
+						redirectURL={resource.redirectURL}
+						sxpElementId={sxpElementId}
+						type={resource.type}
+					/>
+				</ErrorBoundary>
+			</div>
+		</ThemeContext.Provider>
+	);
+}
