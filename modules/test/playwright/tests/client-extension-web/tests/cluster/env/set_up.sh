a8cf66c17756553fbb164eb586ecd7b5bf214387
Iván Zaera Avellón <ivan.zaera@liferay.com>
2025-01-21 11:08:29 +0100

    LPD-41116 SF scripts and logging
    As per https://github.com/brianchandotcom/liferay-portal/pull/157876#issuecomment-2603830043

--- modules/test/playwright/tests/client-extension-web/tests/cluster/env/set_up.sh
+++ modules/test/playwright/tests/client-extension-web/tests/cluster/env/set_up.sh
@@ -2,49 +2,31 @@
 
 CURRENT_DIR_NAME=$(dirname ${BASH_SOURCE[0]})
 
+echo CURRENT_DIR_NAME=${CURRENT_DIR_NAME}
+
 source ${CURRENT_DIR_NAME}/../../../../../env/common.sh
 
-function main {
-	echo "Running default setup"
+function cluster_set_up() {
+	prepare_additional_bundles ${1}
 
-	default_set_up
-
-	echo "Preparing cluster slave"
-
-	prepare_additional_bundles 1
-
-	local slave_home="${LIFERAY_HOME}-1"
-
-	# Set up cluster
-	echo "Configuring Elastic Search"
+	local slave_home="${LIFERAY_HOME}-${1}"
 
 	cp "${CURRENT_DIR_NAME}/com.liferay.portal.search.elasticsearch7.configuration.ElasticsearchConfiguration.config" "${slave_home}/osgi/configs"
-
 	sed -i "s/%LIFERAY_DOCKER_NETWORK_NAME%/${LIFERAY_DOCKER_NETWORK_NAME}/g" "${slave_home}/osgi/configs/com.liferay.portal.search.elasticsearch7.configuration.ElasticsearchConfiguration.config"
 
-	echo "Configuring data directory"
-
 	rm -rf "${slave_home}/data"
-
 	mkdir -p "${slave_home}/data"
-
 	ln -s "${LIFERAY_HOME}/data/document_library" "${slave_home}/data"
-
-	echo "Configuring routes"
 
 	for domain in "${slave_home}/routes/default/dxp/"*
 	do
 		sed -i 's/8080/9080/g' "${domain}"
 	done
 
-	echo "Removing unused directories"
-
 	rm -rf "${slave_home}/elasticsearch-sidecar"
 	rm -rf "${slave_home}/osgi/state"
 	rm -rf "${slave_home}/osgi/tomcat/work"
 	rm -rf "${slave_home}/osgi/work"
-
-	echo "Removing configuration properties in conflict with cluster"
 
 	for node_home in "${LIFERAY_HOME}" "${slave_home}"
 	do
@@ -52,11 +34,15 @@ function main {
 		delete_property "${node_home}" "web.server.https.port"
 	done
 
-	echo "Changing configuration properties in conflict with cluster"
-
 	update_property "${slave_home}" "module.framework.properties.osgi.console" "localhost:11313"
 
-	start_additional_bundles 1
+	start_additional_bundles ${1}
+}
+
+function main {
+	default_set_up
+
+	cluster_set_up 1
 }
 
 main "${@}"
