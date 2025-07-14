d45903c0772d10c2d0f88c1af139a8bb10767619
antonio-ortega <antonio.ortega@liferay.com>
2025-07-14 11:40:09 +0200

    LPD-60118 Adapt tests accordingly to new label

--- modules/test/playwright/tests/frontend-data-set-web/main/advanced.spec.ts
+++ modules/test/playwright/tests/frontend-data-set-web/main/advanced.spec.ts
@@ -215,7 +215,7 @@ test(
 
 				await page
 					.locator('.dropdown-menu')
-					.getByRole('button', {name: 'Edit Filter'})
+					.getByRole('button', {name: 'Show Results'})
 					.click();
 
 				await page
@@ -257,8 +257,8 @@ test(
 				await page.getByLabel('Exclude').check();
 			});
 
-			await test.step('Click "Edit Filter"', async () => {
-				await page.getByRole('button', {name: 'Edit Filter'}).click();
+			await test.step('Click "Show Results"', async () => {
+				await page.getByRole('button', {name: 'Show Results'}).click();
 
 				await page
 					.getByText('This is a description for sample 10.')
@@ -297,7 +297,7 @@ test(
 				await page.getByRole('checkbox', {name: 'Red'}).check();
 				await page.getByRole('checkbox', {name: 'Blue'}).uncheck();
 
-				await page.getByRole('button', {name: 'Edit Filter'}).click();
+				await page.getByRole('button', {name: 'Show Results'}).click();
 
 				await page
 					.getByText('This is a description for sample 1.')
