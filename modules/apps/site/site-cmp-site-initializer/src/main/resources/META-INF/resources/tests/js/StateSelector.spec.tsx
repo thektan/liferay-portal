adc34e6dd836ee1b681e1ddb0291379db1147c39
Carolina Barbosa <carolina.barbosa@liferay.com>
2026-02-06 11:31:30 -0300

    LPD-76950 [FE] Add unit test

--- modules/apps/site/site-cmp-site-initializer/src/main/resources/META-INF/resources/tests/js/StateSelector.spec.tsx
+++ modules/apps/site/site-cmp-site-initializer/src/main/resources/META-INF/resources/tests/js/StateSelector.spec.tsx
@@ -4,17 +4,30 @@
  */
 
 import '@testing-library/jest-dom';
-import {fireEvent, render} from '@testing-library/react';
+import {fireEvent, render, waitFor} from '@testing-library/react';
 import React from 'react';
 
 import StateSelector from '../../js/components/StateSelector';
 
 const mockStates = [
-	{key: 'notStarted', name: 'Not Started', nextStates: ['inProgress']},
-	{key: 'inProgress', name: 'In Progress', nextStates: ['notStarted']},
+	{key: 'blocked', name: 'Blocked', nextStates: ['done', 'inProgress']},
+	{key: 'done', name: 'Done', nextStates: ['inProgress']},
+	{key: 'inProgress', name: 'In Progress', nextStates: ['blocked', 'done']},
+	{
+		key: 'notStarted',
+		name: 'Not Started',
+		nextStates: ['blocked', 'inProgress'],
+	},
 ];
 
-const mockOnChange = jest.fn();
+let resolvePromise: (value: void | PromiseLike<void>) => void;
+
+const mockOnChange = jest.fn(
+	() =>
+		new Promise<void>((value) => {
+			resolvePromise = value;
+		})
+);
 
 describe('StateSelector', () => {
 	it('renders with initial state', () => {
@@ -47,5 +60,64 @@ describe('StateSelector', () => {
 		fireEvent.click(option);
 
 		expect(mockOnChange).toHaveBeenCalled();
+	});
+
+	it('updates next states in the specific display order', async () => {
+		const {getAllByRole, getByText} = render(
+			<StateSelector
+				initialSelectedKey="notStarted"
+				onChange={mockOnChange}
+				states={mockStates}
+			/>
+		);
+
+		fireEvent.click(getByText('Not Started'));
+
+		let options = getAllByRole('option').map(
+			(option) => option.textContent
+		);
+
+		expect(options.length).toBe(3);
+		expect(options[0]).toBe('Not Started');
+		expect(options[1]).toBe('In Progress');
+		expect(options[2]).toBe('Blocked');
+
+		fireEvent.click(getByText('In Progress'));
+
+		await waitFor(() => resolvePromise());
+
+		fireEvent.click(getByText('In Progress'));
+
+		options = getAllByRole('option').map((option) => option.textContent);
+
+		expect(options.length).toBe(3);
+		expect(options[0]).toBe('In Progress');
+		expect(options[1]).toBe('Blocked');
+		expect(options[2]).toBe('Done');
+
+		fireEvent.click(getByText('Blocked'));
+
+		await waitFor(() => resolvePromise());
+
+		fireEvent.click(getByText('Blocked'));
+
+		options = getAllByRole('option').map((option) => option.textContent);
+
+		expect(options.length).toBe(3);
+		expect(options[0]).toBe('In Progress');
+		expect(options[1]).toBe('Blocked');
+		expect(options[2]).toBe('Done');
+
+		fireEvent.click(getByText('Done'));
+
+		await waitFor(() => resolvePromise());
+
+		fireEvent.click(getByText('Done'));
+
+		options = getAllByRole('option').map((option) => option.textContent);
+
+		expect(options.length).toBe(2);
+		expect(options[0]).toBe('In Progress');
+		expect(options[1]).toBe('Done');
 	});
 });
