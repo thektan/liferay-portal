f4ff57b5c6b41cf48b407034aa8a33b6230c75f9
Marco Leo <marco.leo@liferay.com>
2026-03-29 00:04:16 +0100

    LPD-85418 Add new PhoneNumber Object Field Type

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -315,6 +315,7 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Function;
+import java.util.regex.Pattern;
 
 import javax.crypto.spec.SecretKeySpec;
 
@@ -7772,6 +7773,28 @@ public class ObjectEntryLocalServiceImpl
 				}
 			}
 		}
+		else if (objectField.compareBusinessType(
+					ObjectFieldConstants.BUSINESS_TYPE_PHONE_NUMBER)) {
+
+			String phoneNumber = GetterUtil.getString(value);
+
+			if (Validator.isNotNull(phoneNumber) &&
+				!_phoneNumberPattern.matcher(
+					phoneNumber
+				).matches()) {
+
+				_handle(
+					new ObjectEntryValuesException.InvalidPhoneNumber(
+						objectField.getName()),
+					validationErrors);
+			}
+
+			_validateTextMaxLength(
+				DynamicObjectDefinitionTableUtil.getMaxLength(
+					objectField.getBusinessType()),
+				phoneNumber, objectField.getObjectFieldId(),
+				objectField.getName(), validationErrors);
+		}
 		else if (StringUtil.equals(
 					objectField.getDBType(),
 					ObjectFieldConstants.DB_TYPE_STRING)) {
@@ -7988,6 +8011,8 @@ public class ObjectEntryLocalServiceImpl
 		_objectRelationshipLocalServiceSnapshot = new Snapshot<>(
 			ObjectEntryLocalServiceImpl.class,
 			ObjectRelationshipLocalService.class, null);
+	private static final Pattern _phoneNumberPattern = Pattern.compile(
+		"^\\+?[0-9\\s\\-().]{1,50}$");
 
 	private static final Map<Class<?>, Function<Object, Serializable>>
 		_putValueFunctions =
