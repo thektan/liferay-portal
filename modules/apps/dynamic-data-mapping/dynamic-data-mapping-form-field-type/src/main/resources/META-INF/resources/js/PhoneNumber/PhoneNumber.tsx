6cff41cf9c7fe413345ee44c4d07237c9c0db9fc
Kevin Tan <kevin.tan@liferay.com>
2026-04-15 11:22:29 -0700

    LPD-85415 Switch to Picker component

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/js/PhoneNumber/PhoneNumber.tsx
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/js/PhoneNumber/PhoneNumber.tsx
@@ -4,7 +4,7 @@
  */
 
 import ClayButton from '@clayui/button';
-import ClayDropDown from '@clayui/drop-down';
+import {Option, Picker} from '@clayui/core';
 import {ClayInput} from '@clayui/form';
 import ClayIcon from '@clayui/icon';
 import {ReactFieldBase as FieldBase} from 'dynamic-data-mapping-form-field-type/api';
@@ -24,6 +24,33 @@ interface PhoneNumberProps {
 	[key: string]: unknown;
 }
 
+const PickerTrigger = React.forwardRef<
+	HTMLButtonElement,
+	{selectedCountry: CountryInfo & {symbol?: string}} & React.ComponentProps<
+		typeof ClayButton
+	>
+>(({children, selectedCountry, ...otherProps}, ref) => (
+	<ClayButton
+		{...otherProps}
+		className="btn-secondary mr-2"
+		displayType="secondary"
+		ref={ref}
+		style={{minWidth: '90px'}}
+	>
+		<span className="align-items-center d-flex">
+			{selectedCountry.symbol && (
+				<ClayIcon symbol={selectedCountry.symbol} />
+			)}
+
+			<span className="ml-1">+{selectedCountry.idd}</span>
+
+			<span className="inline-item inline-item-after">
+				<ClayIcon symbol="caret-double" />
+			</span>
+		</span>
+	</ClayButton>
+));
+
 const PhoneNumber = ({
 	countries = [],
 	name,
@@ -36,7 +63,6 @@ const PhoneNumber = ({
 	...otherProps
 }: PhoneNumberProps) => {
 	const [localNumber, setLocalNumber] = useState('');
-	const [searchTerm, setSearchTerm] = useState('');
 	const [selectedCountry, setSelectedCountry] = useState<CountryInfo>(
 		countries[0]
 	);
@@ -53,18 +79,6 @@ const PhoneNumber = ({
 			};
 		});
 	}, [countries]);
-
-	const filteredCountries = useMemo(() => {
-		if (!searchTerm) {
-			return countriesWithFlagSymbol;
-		}
-
-		return countriesWithFlagSymbol.filter(
-			(country) =>
-				country.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-				country.idd.includes(searchTerm)
-		);
-	}, [countriesWithFlagSymbol, searchTerm]);
 
 	const handleValueChange = (country: CountryInfo, number: string) => {
 		if (onChange) {
@@ -96,80 +110,47 @@ const PhoneNumber = ({
 	return (
 		<FieldBase {...otherProps} name={name} readOnly={disabled}>
 			<div className="d-flex">
-				<ClayDropDown
-					closeOnClick
-					trigger={
-						<ClayButton
-							className="btn-secondary mr-2"
-							disabled={disabled}
-							displayType="secondary"
-							style={{minWidth: '90px'}}
-						>
-							<span className="align-items-center d-flex">
-								{selectedCountry.symbol && (
-									<ClayIcon symbol={selectedCountry.symbol} />
-								)}
+				<Picker
+					as={PickerTrigger}
+					disabled={disabled}
+					items={countriesWithFlagSymbol}
+					onSelectionChange={(key) => {
+						const country = countriesWithFlagSymbol.find(
+							(c) => c.a2 === key
+						);
 
-								<span className="ml-1">
-									+{selectedCountry.idd}
-								</span>
-
-								<span className="inline-item inline-item-after">
-									<ClayIcon symbol="caret-double" />
-								</span>
-							</span>
-						</ClayButton>
-					}
+						if (country) {
+							setSelectedCountry(country);
+							handleValueChange(country, localNumber);
+						}
+					}}
+					searchable
+					selectedCountry={selectedCountry}
+					selectedKey={selectedCountry.a2}
 				>
-					<div className="p-2">
-						<ClayInput
-							onChange={(event) =>
-								setSearchTerm(event.target.value)
-							}
-							type="text"
-							value={searchTerm}
-						/>
-					</div>
-
-					<ClayDropDown.ItemList className="dropdown-menu-indicator-start">
-						{filteredCountries.map((country) => (
-							<ClayDropDown.Item
-								active={selectedCountry.a2 === country.a2}
-								key={country.a2}
-								onClick={() => {
-									setSearchTerm('');
-									setSelectedCountry(country);
-									handleValueChange(country, localNumber);
-								}}
-							>
-								<span className="dropdown-item-indicator-start">
-									{selectedCountry.a2 === country.a2 && (
-										<ClayIcon symbol="check" />
+					{(country) => (
+						<Option key={country.a2} textValue={country.name}>
+							<div className="autofit-row">
+								<div className="autofit-col">
+									{country.symbol && (
+										<ClayIcon symbol={country.symbol} />
 									)}
-								</span>
-
-								<div className="autofit-row">
-									<div className="autofit-col">
-										{country.symbol && (
-											<ClayIcon symbol={country.symbol} />
-										)}
-									</div>
-
-									<div
-										className="autofit-col"
-										style={{minWidth: '30px'}}
-									>
-										{`+${country.idd}`}
-									</div>
-
-									<div className="autofit-col autofit-col-expand">
-										{country.name}
-									</div>
 								</div>
-							</ClayDropDown.Item>
-						))}
-					</ClayDropDown.ItemList>
-				</ClayDropDown>
+
+								<div
+									className="autofit-col"
+									style={{minWidth: '30px'}}
+								>
+									{`+${country.idd}`}
+								</div>
+
+								<div className="autofit-col autofit-col-expand">
+									{country.name}
+								</div>
+							</div>
+						</Option>
+					)}
+				</Picker>
 
 				<ClayInput
 					className="ddm-field-text form-control"
